5cfc13e6152e0bcc87fa389c75fc2b98910d6b0e
rashedskills <60914192+rashedskills@users.noreply.github.com>
2020-06-15 01:47:47 +0600

    user section update
    add new details filed user section

--- application/views/backend/user/section_add.php
+++ application/views/backend/user/section_add.php
@@ -4,7 +4,17 @@
         <input class="form-control" type="text" name="title" id="title" required>
         <small class="text-muted"><?php echo get_phrase('provide_a_section_name'); ?></small>
     </div>
+    <div class="form-group">
+    	<label><?php echo get_phrase('details'); ?></label>
+    	<textarea rows="4" class="form-control" name="details" id="topics" placeholder="goal, skills, objective, topic"></textarea>
+	</div>
     <div class="text-right">
         <button class = "btn btn-success" type="submit" name="button"><?php echo get_phrase('submit'); ?></button>
     </div>
 </form>
+
+<script type="text/javascript">
+	$(document).ready(function () {
+    initSummerNote(['#topics']);
+    });
+</script>
