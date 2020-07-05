3daf1b985405776b6a31eebd176d9aa197cbc4d7
rashedskills <60914192+rashedskills@users.noreply.github.com>
2020-07-05 13:52:35 +0600

    email model update 8
    update email model 8 times

--- application/models/Email_model.php
+++ application/models/Email_model.php
@@ -47,17 +47,18 @@ class Email_model extends CI_Model {
 		$websiteUrl = 'http://skillsbd.com';
 		$userid 	= $this->session->userdata('user_id');
 		$user_details = $this->user_model->get_all_user($userid)->row_array();
-		//$purchased_courses 	= $this->session->userdata('cart_items');
 		$email_send_from = get_settings('system_email');	
 		$purchase_subject 	 =  "Purchase Confirmation";
 		$purchase_msg 	=	"<h3 style='margin-top: 20px; font-size: 1rem;'>Hi, ".$user_details["first_name"]." ".$user_details["last_name"]."</h3>";
 		$purchase_msg	.=	"<p style='margin-top: 20px;'>Thanks for buying with us. Below is a summary of your recent purchase.</p>";
-		$purchase_msg	.=	"<strong style='padding-bottom: 10px;'>Purchased Course:</strong>";
+		$purchase_msg	.=	"<strong style='padding: 10px 0px; font-size: 14px; text-decoration: underline'>Purchased Course:</strong><br><br>";
 		foreach ($this->session->userdata('cart_items') as $cart_items) {
 		$course_details  = $this->crud_model->get_course_by_id($cart_items)->row_array();
-		$purchase_msg	.= "<strong>".$course_details['title']."</strong>";
-		$purchase_msg 	.= 	"<hr>";
+		$purchase_msg	.= "<span style='font-size: 20px;'>&#8226;</span><strong style='font-size: 14px;'> ".$course_details['title']."</strong><br>";
 		}
+		$purchase_msg 	.= 	"<strong style='padding: 10px 0px; text-decoration: underline; font-size: 14px;'>Customer Details:</strong><br><br>";
+		$purchase_msg 	.= 	"<strong style='padding-bottom: 10px;'>Phone: </strong>".$user_details["phone"];
+		$purchase_msg 	.= 	"<hr>";
 		$purchase_msg 	.= 	"<p style=''>Sent by <a href=".$websiteUrl.">skillsbd</a> 5th floor, 59 House, 04 Road, C Block, Banani, Dhaka-1213, Bangladesh.</p>";
 		$this->send_smtp_mail($purchase_msg, $purchase_subject, $user_details['email'], $email_send_from);
 	}
